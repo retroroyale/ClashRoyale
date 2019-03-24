0197d1a37d332f0733384c614aecb1070cbdbc2b
Incredible <ta.media.apps@gmail.com>
2019-03-24 15:19:44 +0100

    chat

--- ClashRoyale/ClashRoyale/Logic/Clan/StreamEntry/AllianceStreamEntry.cs
+++ ClashRoyale/ClashRoyale/Logic/Clan/StreamEntry/AllianceStreamEntry.cs
@@ -1,11 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using ClashRoyale.Extensions;
+using DotNetty.Buffers;
+using Newtonsoft.Json;
 
 namespace ClashRoyale.Logic.Clan.StreamEntry
 {
     public class AllianceStreamEntry
     {
-        
+        [JsonProperty("type")] public int StreamEntryType { get; set; }
+        [JsonProperty("id")] public int Id { get; set; }
+        [JsonProperty("highId")] public int SenderHighId { get; set; }
+        [JsonProperty("lowId")] public int SenderLowId { get; set; }
+        [JsonProperty("sender_name")] public string SenderName { get; set; }
+        [JsonProperty("sender_role")] public int SenderRole { get; set; }
+        [JsonProperty("creation")] public DateTime CreationDateTime { get; set; }
+
+        [JsonIgnore]
+        public int AgeSeconds => (int)(DateTime.UtcNow - CreationDateTime).TotalSeconds;
+
+        public virtual void Encode(IByteBuffer packet)
+        {
+            packet.WriteVInt(StreamEntryType);
+            packet.WriteVInt(0);
+
+            packet.WriteVInt(Id);
+
+            packet.WriteVInt(SenderHighId);
+            packet.WriteVInt(SenderLowId);
+            packet.WriteVInt(SenderHighId);
+            packet.WriteVInt(SenderLowId);
+
+            packet.WriteScString(SenderName);
+            packet.WriteVInt(0);
+            packet.WriteVInt(SenderRole);
+
+            packet.WriteVInt(0);
+            packet.WriteVInt(0);
+        }
+
+        public void SetSender(Player player)
+        {
+            SenderName = player.Home.Name;
+            SenderId = player.Home.Id;
+            SenderRole = player.Home.AllianceInfo.Role;
+        }
+
+        [JsonIgnore]
+        public long SenderId
+        {
+            get => ((long)SenderHighId << 32) | (SenderLowId & 0xFFFFFFFFL);
+            set
+            {
+                SenderHighId = Convert.ToInt32(value >> 32);
+                SenderLowId = (int)value;
+            }
+        }
     }
 }
---
+++ ClashRoyale/ClashRoyale/Logic/Clan/StreamEntry/Entries/ChatStreamEntry.cs
@@ -0,0 +1,24 @@
+﻿using ClashRoyale.Extensions;
+using DotNetty.Buffers;
+using Newtonsoft.Json;
+
+namespace ClashRoyale.Logic.Clan.StreamEntry.Entries
+{
+    public class ChatStreamEntry : AllianceStreamEntry
+    {
+        public ChatStreamEntry()
+        {
+            StreamEntryType = 2;
+        }
+
+        [JsonProperty("msg")]
+        public string Message { get; set; }
+
+        public override void Encode(IByteBuffer packet)
+        {
+            base.Encode(packet);
+
+            packet.WriteScString(Message); 
+        }
+    }
+}
