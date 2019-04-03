c94caf8fc493e91f292708821618d02fe5255439
Incredible <ta.media.apps@gmail.com>
2019-04-03 15:16:01 +0200

    send response for tv royale

--- ClashRoyale/ClashRoyale/Protocol/Messages/Client/AskForTvContentMessage.cs
+++ ClashRoyale/ClashRoyale/Protocol/Messages/Client/AskForTvContentMessage.cs
@@ -1,4 +1,6 @@
-﻿using ClashRoyale.Logic;
+﻿using ClashRoyale.Extensions;
+using ClashRoyale.Logic;
+using ClashRoyale.Protocol.Messages.Server;
 using DotNetty.Buffers;
 
 namespace ClashRoyale.Protocol.Messages.Client
@@ -8,6 +10,24 @@ namespace ClashRoyale.Protocol.Messages.Client
         public AskForTvContentMessage(Device device, IByteBuffer buffer) : base(device, buffer)
         {
             Id = 14402;
+        }
+
+        public int ClassId { set; get; }
+        public int InstanceId { set; get; }
+
+        public override void Decode()
+        {
+            ClassId = Reader.ReadVInt();
+            InstanceId = Reader.ReadVInt();
+        }
+
+        public override async void Process()
+        {
+            await new RoyalTvContentMessage(Device)
+            {
+                ClassId = ClassId,
+                InstanceId = InstanceId
+            }.Send();
         }
     }
 }
---
+++ ClashRoyale/ClashRoyale/Protocol/Messages/Server/RoyalTvContentMessage.cs
@@ -0,0 +1,24 @@
+﻿using ClashRoyale.Extensions;
+using ClashRoyale.Logic;
+
+namespace ClashRoyale.Protocol.Messages.Server
+{
+    public class RoyalTvContentMessage : PiranhaMessage
+    {
+        public RoyalTvContentMessage(Device device) : base(device)
+        {
+            Id = 24405;
+        }
+
+        public int ClassId { set; get; }
+        public int InstanceId { set; get; }
+
+        public override void Encode()
+        {
+            Writer.WriteVInt(0);
+
+            Writer.WriteVInt(ClassId);
+            Writer.WriteVInt(InstanceId);
+        }
+    }
+}
