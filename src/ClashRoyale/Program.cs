0843874666c3d4113d8fb37ab5b878f0cc5b88e9
Havij khor <moien.king@gmail.com>
2019-04-27 18:29:02 +0430

    Make console title more verbose

--- src/ClashRoyale/Program.cs
+++ src/ClashRoyale/Program.cs
@@ -6,7 +6,7 @@ namespace ClashRoyale
     {
         private static void Main()
         {
-            Console.Title = "ClashRoyale";
+            Console.Title = "ClashRoyale Server Emulator";
 
             Console.WriteLine(
                 "\n______________             ______ ________                     ______     \r\n__  ____/__  /_____ __________  /____  __ \\__________  _______ ___  /____ \r\n_  /    __  /_  __ `/_  ___/_  __ \\_  /_/ /  __ \\_  / / /  __ `/_  /_  _ \\\r\n/ /___  _  / / /_/ /_(__  )_  / / /  _, _// /_/ /  /_/ // /_/ /_  / /  __/\r\n\\____/  /_/  \\__,_/ /____/ /_/ /_//_/ |_| \\____/_\\__, / \\__,_/ /_/  \\___/ \r\n                                                /____/                    \n\n");
