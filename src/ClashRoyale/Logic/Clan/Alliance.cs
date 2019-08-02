2ffdc1e103daed18afc2b5a9664bb11c46ca17f3
Incredible <ta.media.apps@gmail.com>
2019-08-02 14:11:07 +0200

    readded useless expression

--- src/ClashRoyale/Logic/Clan/Alliance.cs
+++ src/ClashRoyale/Logic/Clan/Alliance.cs
@@ -156,7 +156,9 @@ namespace ClashRoyale.Logic.Clan
                     Stream.RemoveAt(0);
 
                 var max = Stream.Count == 0 ? 1 : Stream.Max(x => x.Id);
-                entry.Id = max == int.MaxValue ? 1 : max + 1; // If we ever reach that value... but who knows...
+                entry.Id = Stream.Count > 0
+                    ? max == int.MaxValue ? 1 : max + 1
+                    : 1; // If we ever reach that value... but who knows...
 
                 Stream.Add(entry);
             }
