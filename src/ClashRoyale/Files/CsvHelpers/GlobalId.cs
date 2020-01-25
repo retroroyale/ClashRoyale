895ac1831230f80bbea0bb768c08c803efae2c09
Incredible <46843455+Incr3dible@users.noreply.github.com>
2020-01-25 10:51:23 +0100

    update GlobalId

--- src/ClashRoyale/Files/CsvHelpers/GlobalId.cs
+++ src/ClashRoyale/Files/CsvHelpers/GlobalId.cs
@@ -2,23 +2,19 @@
 {
     public static class GlobalId
     {
-        private const int kReference = 1125899907;
-
         public static int CreateGlobalId(int classId, int instanceId)
         {
-            return classId * 1000000 + instanceId;
+            return classId <= 0 ? 1000000 + instanceId : classId * 1000000 + instanceId;
         }
 
         public static int GetClassId(int globalId)
         {
-            var type = (int) ((kReference * (long) globalId) >> 32);
-            return (type >> 18) + (type >> 31);
+            return globalId / 1000000;
         }
 
         public static int GetInstanceId(int globalId)
         {
-            var referenceT = (int) ((kReference * (long) globalId) >> 32);
-            return globalId - 1000000 * ((referenceT >> 18) + (referenceT >> 31));
+            return globalId % 1000000;
         }
     }
 }
